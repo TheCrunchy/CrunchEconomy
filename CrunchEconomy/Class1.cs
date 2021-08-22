95f9bcbbed7391c59689b5f2d5b8ad7abf5061f0
TheCrunchy <crunchycamsta@gmail.com>
2021-08-22 18:56:08 +0100

    Add project files.

---
+++ CrunchEconomy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrunchEconomy
+{
+    public class Class1
+    {
+    }
+}
